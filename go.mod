f04a9c9327139eb64d77a3c53a06114448851888
mudriyjo <mudriyjo@gmail.com>
2024-06-13 00:35:17 +0400

    add reading enviroment variable

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/mudriyjo/go-rest-template
 go 1.22.2
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-yaml/yaml v2.1.0+incompatible
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
-	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/go-yaml/yaml"
 )
@@ -18,10 +19,13 @@ type Config struct {
 	}
 }
 
-const PathToConfig = "configs/config.yaml"
+const configPath = "configs/config.yaml"
+
+// For Debugging
+// const configDebugPath = "/../../configs/config.yaml"
 
 func GetConfig() Config {
-	f, err := os.Open(PathToConfig)
+	f, err := os.Open(configPath)
 	if err != nil {
 		panic(err)
 	}
@@ -33,5 +37,43 @@ func GetConfig() Config {
 	if err != nil {
 		panic(err)
 	}
+	// Update config using Env
+	readEnv(&cfg)
+
 	return cfg
+}
+
+func readEnv(cfg *Config) {
+	updateConfigFieldByName(cfg, func(cfg *Config, value string) {
+		cfg.Server.Host = value
+	}, "SERVER_HOST")
+	updateConfigFieldByName(cfg, func(cfg *Config, value string) {
+		res, err := strconv.Atoi(value)
+		if err != nil {
+			panic(err)
+		}
+		cfg.Server.Port = res
+	}, "SERVER_PORT")
+
+	updateConfigFieldByName(cfg, func(cfg *Config, value string) {
+		cfg.Database.Host = value
+	}, "DATABASE_HOST")
+	updateConfigFieldByName(cfg, func(cfg *Config, value string) {
+		res, err := strconv.Atoi(value)
+		if err != nil {
+			panic(err)
+		}
+		cfg.Database.Port = res
+	}, "DATABASE_PORT")
+
+	updateConfigFieldByName(cfg, func(cfg *Config, value string) {
+		cfg.Database.DBname = value
+	}, "DATABASE_NAME")
+}
+
+func updateConfigFieldByName(cfg *Config, fnSetter func(cfg *Config, value string), name string) {
+	val, ok := os.LookupEnv(name)
+	if ok {
+		fnSetter(cfg, val)
+	}
 }
